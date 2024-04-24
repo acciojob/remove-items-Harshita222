265fe733a5f354a8be2456f8fea44da0b71a2cba
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-04-24 13:10:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let btn = document.querySelector('input[type="button"][value="Select and Remove"]');
+let select = document.querySelector("#colorSelect");
+
+btn.addEventListener("click", deleted);
+
+function deleted() {
+	let selectedValue = select.option[select.selectedIndex];
+	let selectOption = selectedValue.text;
+    console.log(`Selected value: ${selectedValue}`);
+
+	selectedValue.remove();
+}
